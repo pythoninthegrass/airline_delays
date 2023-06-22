a910c7505fc52fd08624e37334c1bfd8f44686b0
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-06-21 22:12:19 -0500

    Add core logic functions
    Break out delayed flights and their average times into functions

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ else:
 with open('airlines.json') as f:
     output = json.load(f)
 
-# create airport code set
+# initialize airport code set
 airport_codes = set()
 
 # add all airport codes to set
@@ -71,22 +71,39 @@ elif not re.match(r"^20(0[3-9]|1[0-6])$", year):
 # filter json by Airport > Time > Year > <2004>
 filtered_output = [airport for airport in output if airport['Time']['Year'] == int(year)]
 
-# get percentage of delayed flights by Statistics > # of Delays > Security
-delayed_flights = [airport['Statistics']['# of Delays']['Security'] for airport in filtered_output]
-total_flights = [airport['Statistics']['Flights']['Total'] for airport in filtered_output]
-percent_delayed_flights = [delayed_flights[i] / total_flights[i] for i in range(len(delayed_flights))]
-percent_delayed_flights = [round(percent * 100, 2) for percent in percent_delayed_flights]
-average_flights_delayed = sum(percent_delayed_flights) / len(percent_delayed_flights)
 
-# round to 2 decimal places and with a % sign (e.g., 0.07928... > 7.93%)
-average_flights_delayed = round(average_flights_delayed * 100, 2)
-print(f"Average percentage of delayed flights: {average_flights_delayed}%")
+def delayed_flights():
+    """get percentage of delayed flights by Statistics > # of Delays > Security"""
+    delayed_flights = [airport['Statistics']['# of Delays']['Security'] for airport in filtered_output]
+    total_flights = [airport['Statistics']['Flights']['Total'] for airport in filtered_output]
+    percent_delayed_flights = [delayed_flights[i] / total_flights[i] for i in range(len(delayed_flights))]
+    percent_delayed_flights = [round(percent * 100, 2) for percent in percent_delayed_flights]
+    average_flights_delayed = sum(percent_delayed_flights) / len(percent_delayed_flights)
 
-# get average minutes delayed by Statistics > Minutes Delayed > Security
-minutes_delayed = [airport['Statistics']['Minutes Delayed']['Security'] for airport in filtered_output]
-average_minutes_delayed = [minutes_delayed[i] / delayed_flights[i] for i in range(len(minutes_delayed)) if delayed_flights[i] != 0]
-average_minutes_delayed = [round(minutes, 2) for minutes in average_minutes_delayed]
+    # round to 2 decimal places and with a % sign (e.g., 0.07928... > 7.93%)
+    average_flights_delayed = round(average_flights_delayed * 100, 2)
+    print(f"Average percentage of delayed flights: {average_flights_delayed}%")
 
-# round to 2 decimal places et al
-average_minutes_delayed = round(sum(average_minutes_delayed) / len(average_minutes_delayed), 2)
-print(f"Average minutes delayed: {average_minutes_delayed} minutes")
+    return delayed_flights
+
+
+def average_minutes_delayed(flight_info):
+    """get average minutes delayed by Statistics > Minutes Delayed > Security"""
+    minutes_delayed = [airport['Statistics']['Minutes Delayed']['Security'] for airport in filtered_output]
+    average_minutes_delayed = [minutes_delayed[i] / flight_info[i] for i in range(len(minutes_delayed)) if flight_info[i] != 0]
+    average_minutes_delayed = [round(minutes, 2) for minutes in average_minutes_delayed]
+
+    # round to 2 decimal places et al
+    average_minutes_delayed = round(sum(average_minutes_delayed) / len(average_minutes_delayed), 2)
+    print(f"Average minutes delayed: {average_minutes_delayed} minutes")
+
+    return average_minutes_delayed
+
+
+def main():
+    delays = delayed_flights()
+    average_minutes_delayed(delays)
+
+
+if __name__ == "__main__":
+    main()
